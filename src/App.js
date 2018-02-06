483dc753fc4f71ed5e83ffacbc074ebc918dfe88
mattjamesvisuals <mattjamesvisuals@gmail.com>
2018-02-06 12:38:31 -0700

    initial set up

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to HappilyFi!</h1>
+        </header>
+        <p className="App-intro">
+          Send them some happiness today. 
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;

a6a28d8ad25f516b9212ddd9593c708947e9d9d8
Matt James <mattjamesvisuals@gmail.com>
2018-03-06 11:52:10 -0700

    created info modal component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,37 @@ import logo from './img/wink.png';
 import './App.css';
 import Form from './components/Form';
 import Header from './components/Header';
-
+import Modal from './components/Modal';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isOpen: false };
+  }
+
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Header/>
         <div className="jumbotron">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" onMouseOver={this.toggleModal}/>
           <h1 className="App-title">Welcome to HappilyFi!</h1>
         </div>
         <p className="App-intro">
-          HappilyFi Someone and <b>You</b> Today!
+          Send Happy...Get Happy!
         </p>
         <Form/>
+        <Modal show={this.state.isOpen}
+          onClose={this.toggleModal}>
+          To increase your happiness, all you have to do is randomly wish for somebody else to be happy.
+        </Modal>
       </div>
     );
   }
